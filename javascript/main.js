4f4ea862e997f0ba00936d3446be50e221472d44
Mustafa Kutay Ebiclioglu <kutayeb@gmail.com>
2022-03-03 22:38:11 -0500

    Add animation to github icon

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,11 @@
-console.log("hi");
+
+
+let githubIcon = document.querySelector(".icon-wrapper");
+
+githubIcon.addEventListener("mouseenter", ( event ) => {
+    event.target.classList.add("fa-flip");
+}, false);
+
+githubIcon.addEventListener("mouseleave", ( event ) => {
+    event.target.classList.remove("fa-flip");
+});
