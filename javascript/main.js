d731a0a09e4e4d2d42a8c6807e21db5cabf9e648
Mustafa Kutay Ebiclioglu <kutayeb@gmail.com>
2022-03-06 21:51:32 -0500

    Add slider value update

--- javascript/main.js
+++ javascript/main.js
@@ -9,6 +9,7 @@ import {
 
 const githubIcon = document.querySelector(".icon-wrapper");
 const slider = document.querySelector(".menu__slider");
+const sliderValue = document.querySelector(".menu__slider-value");
 const menuIcon = document.querySelector(".header__settings");
 const gradientItem = document.querySelector(".gradient-items");
 
@@ -25,9 +26,12 @@ menuIcon.addEventListener("click", (event) => {
 
 slider.addEventListener("change", (event) => {
     showPixels();
-    showSliderValue();
     showImage();
 });
+
+slider.oninput = () => {
+    sliderValue.innerText = slider.value;
+};
 
 gradientItem.addEventListener("click", (event) => {
     showGradientSelection(event);
